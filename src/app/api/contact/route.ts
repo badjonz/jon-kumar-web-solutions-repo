3c890b91cbf78287f4d9a4cb12f875aabe5bfd3d
Jon Kumar <badjonz@gmail.com>
2026-02-10 10:41:46 -0400

    Testing email

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -111,7 +111,8 @@ export async function POST(request: Request) {
               // @ts-ignore
           await resend.emails.send({
                 from: fromAddressConfirmation, // Use the new fromAddressConfirmation
-                to: 'jonkumar1989@gmail.com', // Temporarily sending client confirmation to your own email for testing        subject: `Thanks for reaching out, ${sanitizedName}!`,
+                to: 'jonkumar1989@gmail.com', // Temporarily sending client confirmation to your own email for testing
+            subject: `Thanks for reaching out, ${sanitizedName}!`,        subject: `Thanks for reaching out, ${sanitizedName}!`,
         html: `
           <h2>Message Received</h2>
           <p>Hi ${sanitizedName},</p>
